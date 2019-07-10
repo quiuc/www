2ec77704c65ad375301d7573e4a9a1480242b5b1
Jenn Mueng <ldrwarlock@gmail.com>
2019-07-09 22:08:28 -0500

    added About to header, hide People on small mobile

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,22 +4,25 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = props => {
-    return (
-        <div className="headerContainer">
-            <img className="headerLogo" src="assets/logo.svg" alt="QUIUC" />
-            <li className="headerLinks">
-                <Link to="people">
-                    <div className="headerLink">People</div>
-                </Link>
-                <Link to="calendar">
-                    <div className="headerLink">Calendar</div>
-                </Link>
-                <Link to="contact">
-                    <div className="headerLink">Contact</div>
-                </Link>
-            </li>
-        </div>
-    );
+	return (
+		<div className="headerContainer">
+			<img className="headerLogo" src="assets/logo.svg" alt="QUIUC" />
+			<li className="headerLinks">
+				<Link to="about">
+					<div className="headerLink">About</div>
+				</Link>
+				<Link to="people">
+					<div className="peopleLink headerLink">People</div>
+				</Link>
+				<Link to="calendar">
+					<div className="headerLink">Calendar</div>
+				</Link>
+				<Link to="contact">
+					<div className="headerLink">Contact</div>
+				</Link>
+			</li>
+		</div>
+	);
 };
 
 export default Header;
