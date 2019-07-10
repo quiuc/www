af6da215c687dc5da206afd606c5e4f8f40264bf
Jenn Mueng <ldrwarlock@gmail.com>
2019-07-09 22:05:02 -0500

    added about section

---
+++ src/Pages/Home/Components/About.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import Markdown from 'react-markdown';
+
+import './About.css';
+import aboutMdRef from '../resources/about.md';
+import workshopsMdRef from '../resources/workshops.md';
+import researchMdRef from '../resources/research.md';
+import Button from '../../../Components/boilerplate/Button';
+
+const About = () => {
+	const [aboutMd, setAboutMd] = useState(null);
+	const [workshopsMd, setWorkshopsMd] = useState(null);
+	const [researchMd, setResearchMd] = useState(null);
+
+	useEffect(() => {
+		fetch(aboutMdRef)
+			.then(md => md.text())
+			.then(text => setAboutMd(text));
+
+		fetch(workshopsMdRef)
+			.then(md => md.text())
+			.then(text => setWorkshopsMd(text));
+
+		fetch(researchMdRef)
+			.then(md => md.text())
+			.then(text => setResearchMd(text));
+	});
+
+	return (
+		<>
+			<div className="sectionContainer">
+				<div className="aboutSection">
+					<Markdown source={aboutMd} />
+				</div>
+				<div className="aboutSection">
+					<Markdown source={workshopsMd} />
+					<div className="aboutButtonSpacing" />
+					<Button text="Check out our workshops" />
+				</div>
+				<div className="aboutSection">
+					<Markdown source={researchMd} />
+					<div className="aboutButtonSpacing" />
+					<Button text="Check out our projects" />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default About;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,27 +3,28 @@ import PropTypes from 'prop-types';
 
 import './Home.css';
 import Header from '../../Components/Header/Header';
-import Hero from './Components/Hero.jsx';
+import Hero from './Components/Hero';
+import About from './Components/About';
 
 const Home = props => {
-    // use the routeName to determine where to scroll to.
-    const [routeName] = useState(props.location.pathname.split('/')[1]);
+	// use the routeName to determine where to scroll to.
+	const [routeName] = useState(props.location.pathname.split('/')[1]);
 
-    return (
-        <div className="screenContainer">
-            <div className="background" />
-            <Header />
-            <div className="headerOffset" />
-            <Hero />
-            <div className="sectionContainer" />
-        </div>
-    );
+	return (
+		<div className="screenContainer">
+			<div className="background" />
+			<Header />
+			<div className="headerOffset" />
+			<Hero />
+			<About />
+		</div>
+	);
 };
 
 Home.propTypes = {
-    location: {
-        pathname: PropTypes.string.isRequired
-    }
+	location: {
+		pathname: PropTypes.string.isRequired
+	}
 };
 
 export default Home;
