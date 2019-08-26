7298cf87b1f89a30acd47c25991663db43f525bc
Bailey Tincher <btincher99@gmail.com>
2019-08-26 12:19:52 -0500

    Dropping people

--- src/Pages/Home/Components/People.jsx
+++ src/Pages/Home/Components/People.jsx
@@ -9,9 +9,7 @@ const People = (props, ref) => {
 			<h1>People</h1>
 			<div className="peopleList">
 				<Person name="Bailey Tincher" position="President" imageSrc="/assets/people/bailey.jpg" />
-				<Person name="Guangkuo Liu" position="Education Team Chair" imageSrc="/assets/people/guangkuo.jpg" />
-				<Person name="Léo Loubières" position="Education Team Co-Chair" imageSrc="/assets/people/leo.jpg" />
-				<Person name="Tracy Tang" position="Outreach Chair" imageSrc="/assets/people/tracy.jpg" />
+				<Person name="Guangkuo Liu" position="Education Committee" imageSrc="/assets/people/guangkuo.jpg" />
 				<Person name="Jenn Mueng" position="Web Development" imageSrc="/assets/people/jenn.jpg" />
 			</div>
 		</div>
